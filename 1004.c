3e0fde05d360349a2e9bfd864fc82326a012f777
Md. Fazle Rabbi <fajlerabbiasik@gmail.com>
2024-02-07 19:52:47 +0600

    Simple Product

---
+++ 1004.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int A, B, multiple;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    multiple = A * B;
+
+    printf("PROD = %d\n", multiple);
+
+    return 0;
+}
