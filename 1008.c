4face19cc89312c98a9eccdc477c2616939c28b8
Md. Fazle Rabbi <fajlerabbiasik@gmail.com>
2024-02-07 20:52:14 +0600

    Salary

---
+++ 1008.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int number, work_hour;
+    double amount;
+
+    scanf("%d", &number);
+    scanf("%d", &work_hour);
+    scanf("%lf", &amount);
+
+    printf("NUMBER = %d\nSALARY = U$ %.2lf", number, work_hour * amount);
+
+    return 0;
+}
