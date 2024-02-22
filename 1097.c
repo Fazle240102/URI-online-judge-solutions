ca578608661a78d5ca624833034cebea993521d0
Md. Fazle Rabbi <fajlerabbiasik@gmail.com>
2024-02-22 22:12:15 +0600

    Sequence IJ 3

--- 1097.c
+++ 1097.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int i, j;
+    int i, j, j_init = 7;
     for (i = 1; i <= 9; i += 2)
     {
 
-        for (j = 7; j >= 5; j -= 1)
+        for (j = j_init; j >= j_init - 2; j -= 1)
         {
 
             printf("I=%d J=%d\n", i, j);
         }
+
+        j_init += 2;
     }
     return 0;
 }
