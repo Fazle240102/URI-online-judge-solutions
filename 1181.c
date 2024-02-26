33667e5e3ba75c40729b9ee5d3f287535d1658c9
Md. Fazle Rabbi <fajlerabbiasik@gmail.com>
2024-02-26 19:32:22 +0600

    Line in Array

---
+++ 1181.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    double x[12][12], sum = 0;
+    int i, j, k, n;
+    char ch;
+
+    scanf("%d %c", &n, &ch);
+    for (j = 0; j < 12; j++)
+    {
+        for (k = 0; k < 12; k++)
+        {
+            scanf("%lf", &x[j][k]);
+        }
+    }
+
+    if (ch == 'S')
+    {
+        for (i = 0; i < 12; i++)
+        {
+            scanf("%lf", &x[n][i]);
+            sum += x[n][i];
+        }
+        printf("%.1lf\n", sum);
+    }
+    else if (ch == 'M')
+    {
+        for (i = 0; i < 12; i++)
+        {
+            scanf("%lf", &x[n][i]);
+            sum += x[n][i];
+        }
+        printf("%.1f\n", sum / 12.0);
+    }
+}
