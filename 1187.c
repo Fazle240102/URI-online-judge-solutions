07616cb7978cdec11fc3c786880020ce2520fa0d
Md. Fazle Rabbi <fajlerabbiasik@gmail.com>
2024-02-26 23:16:22 +0600

    Top Area

---
+++ 1187.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    double x[12][12], sum = 0;
+    int i, j, k, m;
+    char ch;
+
+    scanf("%c", &ch);
+    for (j = 0; j < 12; j++)
+    {
+        for (k = 0; k < 12; k++)
+        {
+            scanf("%lf", &x[j][k]);
+        }
+    }
+
+    if (ch == 'S')
+    {
+        for (i = 0; i < 5; i++)
+        {
+
+            for (m = 1 + i; m < 11 - i; m++)
+            {
+                sum += x[i][m];
+            }
+        }
+        printf("%.1lf\n", sum);
+    }
+    else if (ch == 'M')
+    {
+        for (i = 0; i < 5; i++)
+        {
+
+            for (m = 1 + i; m < 11 - i; m++)
+            {
+                sum += x[i][m];
+            }
+        }
+        printf("%.1f\n", sum / 30.0);
+    }
+}
